d69bd6ca5d2966ded5e3f43fa14478d2d51200dd
BodneyC <benjcarrington@gmail.com>
2020-12-11 22:08:41 +0000

    fix(plugin-path): Using packer, not vim-plug

--- alter-fzf-preview.sh
+++ alter-fzf-preview.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FZF_PREVIEW_SCRIPT="$HOME/.local/share/nvim/plugged/fzf.vim/bin/preview.sh"
+FZF_PREVIEW_SCRIPT="$HOME/.local/share/nvim/site/pack/packer/start/fzf.vim/bin/preview.sh"
 
 [[ -f "$FZF_PREVIEW_SCRIPT" ]] && \
   sed -i 's/REVERSE=.*/REVERSE="\\x1b\[3m\\x1b\[1m"/' "$FZF_PREVIEW_SCRIPT"
