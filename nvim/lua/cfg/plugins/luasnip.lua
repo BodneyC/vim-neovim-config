39d8be60ec1dd615a8a5b5feec5a4a77270a6bb7
BodneyC <benjcarrington@gmail.com>
2025-03-27 21:12:37 +0000

    feat(snippets): addition of friendly snippets

--- nvim/lua/cfg/plugins/luasnip.lua
+++ nvim/lua/cfg/plugins/luasnip.lua
@@ -1,17 +1,3 @@
--- local function snippet_map(mode, key, pos)
---   vim.keymap.set(
---     mode,
---     key,
---     [[<cmd>lua require'luasnip'.jump(]] .. pos .. [[)<Cr>]],
---     { silent = true }
---   )
--- end
-
--- snippet_map('i', '<C-j>', 1)
--- snippet_map('i', '<C-k>', -1)
--- snippet_map('s', '<C-j>', 1)
--- snippet_map('s', '<C-k>', -1)
-
 local ls = require("luasnip")
 
 vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(1) end, { silent = true })
@@ -26,4 +12,6 @@ end, { silent = true })
 require('luasnip.loaders.from_snipmate').lazy_load({
   paths = os.getenv('HOME') .. '/.config/nvim/snippets',
 })
-require("luasnip.loaders.from_vscode").load({ path = os.getenv('HOME') .. '/gitclones/friendly-snippets' })
+require("luasnip.loaders.from_vscode").load({
+  path = os.getenv('HOME') .. '/.config/nvim/snippets/friendly-snippets',
+})
